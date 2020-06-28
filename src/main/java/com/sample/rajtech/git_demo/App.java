793950348f35c25e00aa9e7a39fb8150750d7485
rajsachdeva <rsachdeva516@gmail.com>
2020-06-28 19:44:35 +0800

    add if condition to check the data

--- src/main/java/com/sample/rajtech/git_demo/App.java
+++ src/main/java/com/sample/rajtech/git_demo/App.java
@@ -9,6 +9,9 @@ public class App
 	
 	public void addData(String data) {
 		System.out.println("data:"+data);
+		if(data.equals("java technology")) {
+			System.out.println("Welcome to git tutorial");
+		}
 	}
     public static void main( String[] args )
     {
